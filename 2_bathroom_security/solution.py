2fe64f58c82aecebafa4ec7ca4676117a55c1372
Anatoli Makarevich <makaroni4@gmail.com>
2016-12-31 17:13:50 +0100

    Added solution to 2nd part of 2nd puzzle

--- 2_bathroom_security/solution.py
+++ 2_bathroom_security/solution.py
@@ -2,18 +2,27 @@ import numpy as np
 
 def decode(instructions):
   code = []
-  current_index = [1, 1]
-  KEYPAD = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+  # current_index = [1, 1]
+  # KEYPAD = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  current_index = [0, 2]
+  KEYPAD = np.array([
+    [None, None, 1, None, None],
+    [None, 2, 3, 4, None],
+    [5, 6, 7, 8, 9],
+    [None, "A", "B", "C", None],
+    [None, None, "D", None, None]
+  ])
 
   for instruction in instructions:
     for command in instruction:
-      if command == "R" and current_index[0] < 2:
+      if command == "R" and len(KEYPAD[current_index[1]]) > current_index[0] + 1 and KEYPAD[current_index[1]][current_index[0] + 1]:
         current_index[0] += 1
-      elif command == "L" and current_index[0] > 0:
+      elif command == "L" and current_index[0] > 0 and KEYPAD[current_index[1]][current_index[0] - 1]:
         current_index[0] -= 1
-      elif command == "U" and current_index[1] > 0:
+      elif command == "U" and current_index[1] > 0 and KEYPAD[current_index[1] - 1][current_index[0]]:
         current_index[1] -= 1
-      elif command == "D" and current_index[1] < 2:
+      elif command == "D" and len(KEYPAD) > current_index[1] + 1 and KEYPAD[current_index[1] + 1][current_index[0]]:
         current_index[1] += 1
 
     code.append(KEYPAD[current_index[1]][current_index[0]])
