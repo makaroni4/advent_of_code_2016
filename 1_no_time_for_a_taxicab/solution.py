3d46352b8776525a8a1fb6924dc39275035c5fc9
Anatoli Makarevich <makaroni4@gmail.com>
2016-12-31 16:39:59 +0100

    Added solution to 2nd part of 1st puzzle

--- 1_no_time_for_a_taxicab/solution.py
+++ 1_no_time_for_a_taxicab/solution.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def to_key(array):
+  return ";".join(map(str, array))
+
 def calculate_distance(steps):
   # FYI: https://en.wikipedia.org/wiki/Rotation_matrix
   RIGHT_TURN_MATRIX = np.array([[0, 1], [-1, 0]])
@@ -7,14 +10,24 @@ def calculate_distance(steps):
 
   coordinates = np.array([0, 0])
   direction = np.array([0, 1])
+  visited_locations = set(coordinates)
+  visited_twice = False
 
   for step in steps:
     distance = int(step[1:])
 
     direction = RIGHT_TURN_MATRIX.dot(direction) if step[0] == "R" else LEFT_TURN_MATRIX.dot(direction)
-    coordinates = coordinates + direction.dot(distance)
 
-  print np.absolute(coordinates).sum()
+    for i in range(0, distance):
+      coordinates = coordinates + direction
+
+      if not visited_twice and to_key(coordinates) in visited_locations:
+        visited_twice = True
+        print "First location visited twice is %d blocks away" % np.absolute(coordinates).sum()
+      else:
+        visited_locations.add(to_key(coordinates))
+
+  print "Bunny HQ is %d blocks away" % np.absolute(coordinates).sum()
 
 # calculate_distance(["R2"])
 # calculate_distance(["L2"])
@@ -22,6 +35,7 @@ def calculate_distance(steps):
 # calculate_distance(["R2", "R2", "R2"])
 # calculate_distance(["R5", "L5", "R5", "R3"])
 # calculate_distance(["R2", "R2", "R2", "R2"])
+# calculate_distance(["R8", "R4", "R4", "R8"])
 
 steps = open("input.dat").read().strip().split(", ")
 calculate_distance(steps)
