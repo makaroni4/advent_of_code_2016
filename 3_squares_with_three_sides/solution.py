4dbfcada53a6f8b8de706952b40607c0bf5bf6ba
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-01 12:52:12 +0100

    Added solutions to 2nd part of 3rd puzzle

--- 3_squares_with_three_sides/solution.py
+++ 3_squares_with_three_sides/solution.py
@@ -16,8 +16,25 @@ assert is_valid_triangle([3, 4, 5]) == True
 assert is_valid_triangle([5, 10, 25]) == False
 assert count_valid_triangles([[3, 4, 5], [5, 10, 25]]) == 1
 
-
 triangles = open("input.dat").read().strip().split("\n")
 triangles = [map(int, re.split(r"\s+", triangle.strip())) for triangle in triangles]
 print count_valid_triangles(triangles)
 
+# https://docs.python.org/3.1/library/itertools.html#recipes
+def grouper(n, iterable, fillvalue=None):
+  args = [iter(iterable)] * n
+  return itertools.izip_longest(*args, fillvalue=fillvalue)
+
+def count_valid_vertical_triangles(triangles):
+  valid_triangles = 0
+
+  for triangles_group in grouper(3, triangles):
+    for triangle in map(list, zip(*triangles_group)):
+      if is_valid_triangle(triangle):
+        valid_triangles += 1
+
+  return valid_triangles
+
+assert count_valid_vertical_triangles([[3, 10, 5], [4, 25, 3], [5, 5, 4]]) == 2
+
+print count_valid_vertical_triangles(triangles)
