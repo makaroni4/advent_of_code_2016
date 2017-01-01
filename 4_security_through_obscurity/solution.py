c8d2af5242b6464230056ad161bfaa688c78b233
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-01 14:05:01 +0100

    Added solutions for 1st part of 4th puzzle

--- 4_security_through_obscurity/solution.py
+++ 4_security_through_obscurity/solution.py
@@ -1,7 +1,46 @@
-def is_room_real(name):
-  return True
+from collections import defaultdict
 
-assert is_room_real("aaaaa-bbb-z-y-x-123[abxyz]") == True
-assert is_room_real("a-b-c-d-e-f-g-h-987[abcde]") == True
-assert is_room_real("not-a-real-room-404[oarel]") == True
-assert is_room_real("totally-real-room-200[decoy]") == False
+def chars_dict_compare(i1, i2):
+  if i2[1] > i1[1]:
+    return 1
+  elif i2[1] < i1[1]:
+    return -1
+  elif i2[1] == i1[1]:
+    if i2[0] > i1[0]:
+      return -1
+    elif i2[0] < i1[0]:
+      return 1
+    else:
+      return 0
+
+def check_room(name):
+  check_sum = name[-6:-1]
+  encrypted_name, sector_id = name[0:-7].rsplit("-", 1)
+  encrypted_name = encrypted_name.replace("-", "")
+
+  chars = defaultdict(int)
+  for char in encrypted_name:
+    chars[char] += 1
+
+  chars = sorted(chars.items(), cmp=chars_dict_compare)
+
+  for index, char in enumerate(check_sum):
+    if chars[index][0] != char:
+      return -1
+
+  return int(sector_id)
+
+assert check_room("aaaaa-bbb-z-y-x-123[abxyz]") == 123
+assert check_room("a-b-c-d-e-f-g-h-987[abcde]") == 987
+assert check_room("not-a-real-room-404[oarel]") == 404
+assert check_room("totally-real-room-200[decoy]") == -1
+
+names = open("input.dat").read().strip().split("\n")
+
+ids_sum = 0
+for name in names:
+  sector_id = check_room(name)
+  if sector_id > 0:
+    ids_sum += sector_id
+
+print ids_sum
