ecded0bcee5ddff90455d84d487876f76db62771
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-02 13:11:02 +0100

    Added solution to 2nd part of 6th puzzle

--- 6_signals_and_noise/solution.py
+++ 6_signals_and_noise/solution.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import operator
 
-def decode(signals):
+def decode(signals, most_common=True):
   message = ""
   column_signals = zip(*signals)
 
@@ -11,12 +11,15 @@ def decode(signals):
     for char in signal:
       chars[char] += 1
 
-    sorted_letters = sorted(chars.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_letters = sorted(chars.items(), key=operator.itemgetter(1), reverse=most_common)
     message += sorted_letters[0][0]
 
   return message
 
-assert decode(["eedadn" ,"drvtee" ,"eandsr" ,"raavrd" ,"atevrs" ,"tsrnev" ,"sdttsa" ,"rasrtv" ,"nssdts" ,"ntnada" ,"svetve" ,"tesnvt" ,"vntsnd" ,"vrdear" ,"dvrsen" ,"enarar"]) == "easter"
+assert decode(["eedadn" ,"drvtee" ,"eandsr" ,"raavrd" ,"atevrs" ,"tsrnev" ,"sdttsa" ,"rasrtv" ,"nssdts" ,"ntnada" ,"svetve" ,"tesnvt" ,"vntsnd" ,"vrdear" ,"dvrsen" ,"enarar"], True) == "easter"
 
 signals = open("input.dat").read().strip().split("\n")
-print decode(signals)
+print decode(signals, True)
+
+assert decode(["eedadn" ,"drvtee" ,"eandsr" ,"raavrd" ,"atevrs" ,"tsrnev" ,"sdttsa" ,"rasrtv" ,"nssdts" ,"ntnada" ,"svetve" ,"tesnvt" ,"vntsnd" ,"vrdear" ,"dvrsen" ,"enarar"], False) == "advent"
+print decode(signals, False)
