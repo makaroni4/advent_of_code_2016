8c613d1beca9281b75a7b1c52f196bd93579f5b5
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-06 22:45:46 +0100

    Added reading commands for display in 8th puzzle

--- 8_two_factor_authentication/solution.py
+++ 8_two_factor_authentication/solution.py
@@ -1,3 +1,6 @@
+def flatten(l):
+  return [item for sublist in l for item in sublist]
+
 def rect(display, width, height):
   for y in range(0, height):
     display[y][0:width] = ["*"] * width
@@ -18,22 +21,60 @@ def rotate_array(array, shift):
 assert rotate_array(['#', '.', '#', '.', '.', '.', '.'], 1) == ['.', '#', '.', '#', '.', '.', '.']
 assert rotate_array(['#', '.', '#', '.', '.', '.', '.'], 4) == ['.', '.', '.', '.', '#', '.', '#']
 
-def shift_row(display, index, shift):
+def rotate_row(display, index, shift):
   display[index] = rotate_array(display[index], shift)
 
   return display
 
-assert shift_row([['#', '.', '#', '.', '.', '.', '.'],
-                  ['.', '.', '.', '.', '.', '.', '.'],
-                  ['.', '.', '.', '.', '.', '.', '.']], 0, 4) == [['.', '.', '.', '.', '#', '.', '#'],
-                                                                  ['.', '.', '.', '.', '.', '.', '.'],
-                                                                  ['.', '.', '.', '.', '.', '.', '.']]
+assert rotate_row([['#', '.', '#', '.', '.', '.', '.'],
+                   ['.', '.', '.', '.', '.', '.', '.'],
+                   ['.', '.', '.', '.', '.', '.', '.']], 0, 4) == [['.', '.', '.', '.', '#', '.', '#'],
+                                                                   ['.', '.', '.', '.', '.', '.', '.'],
+                                                                   ['.', '.', '.', '.', '.', '.', '.']]
 
 def rotate_column(display, column, shift):
-  return map(list, zip(*shift_row(zip(*display), column, shift)))
+  return map(list, zip(*rotate_row(zip(*display), column, shift)))
 
 assert rotate_column([['*', '*', '*', '.', '.', '.', '.'],
                       ['*', '*', '*', '.', '.', '.', '.'],
                       ['.', '.', '.', '.', '.', '.', '.']], 1, 1) == [['*', '.', '*', '.', '.', '.', '.'],
                                                                       ['*', '*', '*', '.', '.', '.', '.'],
                                                                       ['.', '*', '.', '.', '.', '.', '.']]
+
+def rotate_display(display, direction, coordinate, shift):
+  method = eval("rotate_" + direction)
+  return method(display, coordinate, shift)
+
+display = [["."] * 50] * 6
+
+commands = open("input.dat").read().strip().split("\n")
+
+for command in commands:
+  command_parts = command.split(" ")
+
+  command_name = command_parts.pop(0)
+
+  if command_name == "rect":
+    x, y = map(int, command_parts.pop(0).split("x"))
+
+    display = rect(display, x, y)
+  elif command_name == "rotate":
+    direction, coordinate, by, value = command_parts
+    coordinate = int(coordinate.split("=")[-1])
+
+    display = rotate_display(display, direction, coordinate, int(value))
+  else:
+    raise ValueError("Unknown command " + command)
+
+print sum([1 for pixel in flatten(display) if pixel == "*"])
+
+
+
+
+
+
+
+
+
+
+
