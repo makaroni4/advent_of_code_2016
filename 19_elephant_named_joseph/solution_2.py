c02d140d7516610d3612250a4cf701cf4e15629d
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-13 00:45:23 +0100

    Added fast solution for 2nd part of 19th puzzle

--- 19_elephant_named_joseph/solution_2.py
+++ 19_elephant_named_joseph/solution_2.py
@@ -1,14 +1,41 @@
-import math
+import collections
 
-def brute_force_find_lucky_elf(queue):
+def dummy_find_lucky_elf(elves):
+  queue = list(range(1, elves + 1))
+
   while len(queue) > 1:
-    queue.pop(int(len(queue) / 2))
+    queue.pop(len(queue) // 2)
 
     current_elve = queue.pop(0)
     queue.append(current_elve)
 
   return queue[0]
 
-assert brute_force_find_lucky_elf(list(range(1, 6))) == 2
-assert brute_force_find_lucky_elf(list(range(1, 5))) == 1
+assert dummy_find_lucky_elf(4) == 1
+assert dummy_find_lucky_elf(5) == 2
+assert dummy_find_lucky_elf(6) == 3
+assert dummy_find_lucky_elf(10) == 1
+assert dummy_find_lucky_elf(177) == 111
 
+def fast_find_lucky_elf(elves):
+  queue1 = collections.deque(range(1, 1 + elves // 2))
+  queue2 = collections.deque(range(1 + elves // 2, elves + 1))
+
+  while queue1:
+    current = queue1.popleft()
+
+    queue2.popleft()
+    queue2.append(current)
+
+    if len(queue2) > len(queue1) + 1:
+      queue1.append(queue2.popleft())
+
+  return queue2[0]
+
+assert fast_find_lucky_elf(4) == 1
+assert fast_find_lucky_elf(5) == 2
+assert fast_find_lucky_elf(6) == 3
+assert fast_find_lucky_elf(10) == 1
+assert fast_find_lucky_elf(177) == 111
+
+print(fast_find_lucky_elf(3014603))
