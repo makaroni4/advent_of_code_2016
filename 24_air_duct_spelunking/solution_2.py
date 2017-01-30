a53bada0f377abae763c09e27f72a095e48db89b
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-31 00:34:40 +0100

    Added A star solution for 2nd part of 22nd puzzle

---
+++ 24_air_duct_spelunking/solution_2.py
@@ -0,0 +1,102 @@
+from heapq import *
+import operator
+import itertools
+
+def is_checkpoint(nodes, position):
+  el = nodes[position]
+
+  return el != "." and el != "#"
+
+def read_input(file_name):
+  rows = open(file_name).read().strip().split("\n")[1:-1]
+  rows = list(map(lambda r: r[1:-1], rows))
+
+  nodes = {}
+  initial_position = ()
+  total_checkpoints = 0
+
+  for i, row in enumerate(rows):
+    for j, el in enumerate(row):
+      nodes[(i, j)] = el
+
+      if el == "0":
+        initial_position = (i, j)
+
+      if is_checkpoint(nodes, (i, j)):
+        total_checkpoints += 1
+
+  return (nodes, initial_position, total_checkpoints)
+
+def is_closed_space(nodes, node):
+  return nodes[node] == "#"
+
+def dist(x, y):
+  return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
+assert dist((1, 2), (6, 7)) == 10
+
+def heuristic(initial_position, current_position, visited_points, total_checkpoints):
+  return total_checkpoints - len(visited_points) + dist(initial_position, current_position)
+
+def reconstruct_path(current, came_from):
+  shortest_path = []
+
+  while current in came_from:
+    shortest_path.append(current)
+    current = came_from[current]
+
+  return shortest_path
+
+def astar(nodes, initial_position, total_checkpoints):
+  neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+  counter = itertools.count()
+
+  came_from = {}
+  visited_check_points = ""
+  g_scores = { (initial_position, visited_check_points): 0 }
+  open_set = []
+
+  heappush(open_set, (heuristic(initial_position, initial_position, visited_check_points, total_checkpoints), next(counter), initial_position, visited_check_points))
+
+  while open_set:
+    _, _, position, visited_check_points = heappop(open_set)
+
+    if heuristic(initial_position, position, visited_check_points, total_checkpoints) == 0:
+      shortest_path = reconstruct_path((position, visited_check_points), came_from)
+      return len(shortest_path)
+
+    for next_step in neighbors:
+      next_position = tuple(map(operator.add, position, next_step))
+
+      if next_position not in nodes:
+        continue
+
+      if is_closed_space(nodes, next_position):
+        continue
+
+      next_visited_check_points = str(visited_check_points)
+      if is_checkpoint(nodes, next_position) and not nodes[next_position] in visited_check_points:
+        next_visited_check_points = "".join(sorted(visited_check_points + nodes[next_position]))
+
+      current_state = (position, visited_check_points)
+      next_state = (next_position, next_visited_check_points)
+
+      tentative_g_score = g_scores[current_state] + 1
+
+      if not next_state in g_scores or \
+        tentative_g_score < g_scores[next_state]:
+
+        came_from[next_state] = current_state
+        g_scores[next_state] = tentative_g_score
+
+        f_score = tentative_g_score + heuristic(initial_position, position, next_visited_check_points, total_checkpoints)
+        heappush(open_set, (f_score, next(counter), next_position, next_visited_check_points))
+
+  return False
+
+test_params = read_input("test_input.dat")
+assert astar(*test_params) == 20
+
+params = read_input("input.dat")
+print(astar(*params))
